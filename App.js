86c8056bf7b57b04b540b570a4ed67ab9a66d583
Vasanth <vasanthbonagiri@gmail.com>
2026-02-07 20:06:49 +0530

    fixed parcel setup and react rendering

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello From React, to get hooked 🚀"
+);
 
-
-
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "Iam an h1 tag"),
-        React.createElement("h2", {}, "Iam an h2 tag")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "Iam an h1 tag"),
-        React.createElement("h2", {}, "Iam an h2 tag")
-    ])
-]);
-
-
-const heading = React.createElement("h1", {id:"heading"}, "Hello From React, to get hoookedd");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(heading);
---
+++ dist/Namaste-react.6bd02f5a.js
@@ -0,0 +1,23 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "Iam an h1 tag"),
+        React.createElement("h2", {}, "Iam an h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "Iam an h1 tag"),
+        React.createElement("h2", {}, "Iam an h2 tag")
+    ])
+]);
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello From React, to get hoookedd");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=Namaste-react.6bd02f5a.js.map
